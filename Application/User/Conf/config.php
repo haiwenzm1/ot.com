10c94183cbb2249193cc00fc9a351de3ccb341c6
bnqboy <Haiwen.Ji@b-and-qchina.com>
2017-03-15 16:03:27 +0800

    style

--- Application/User/Conf/config.php
+++ Application/User/Conf/config.php
@@ -15,5 +15,6 @@
 define('UC_APP_ID', 1); //应用ID
 define('UC_API_TYPE', 'Model'); //可选值 Model / Service
 define('UC_AUTH_KEY', '^pfYzjasWq5?7>!DJEKPM#%-IABH"n,2Rmt.gk}C'); //加密KEY
+// define('UC_AUTH_KEY', 'm26L0et.M?HD+-gK/VSf4|k>]UvFJibya(7:O~*;'); //加密KEY
 define('UC_DB_DSN', 'mysqli://root:123456@127.0.0.1:3306/ot_com'); // 数据库连接，使用Model方式调用API必须配置此项
 define('UC_TABLE_PREFIX', 'ot_'); // 数据表前缀，使用Model方式调用API必须配置此项
