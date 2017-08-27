53c5a8c64c4abb65208421f7281912f0f373a1d7
bnqboy <Haiwen.Ji@b-and-qchina.com>
2017-08-27 21:22:57 +0800

    index

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -30,4 +30,17 @@ class IndexController extends HomeController {
         $this->display();
     }
 
+    public function whoWeAre(){
+        
+                $category = D('Category')->getTree();
+                $lists    = D('Document')->lists(null);
+        
+                $this->assign('category',$category);//栏目
+                $this->assign('lists',$lists);//列表
+                $this->assign('page',D('Document')->page);//分页
+        
+                         
+                $this->display();
+            }
+
 }
