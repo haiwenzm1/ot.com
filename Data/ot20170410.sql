51d3b12ee1f8166ce4156792a7047760dea57d00
win7 <896582248@qq.com>
2017-08-31 23:00:45 +0800

    图片优化

--- Data/ot20170410.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE ot_product_category ADD COLUMN nickname VARCHAR(512) DEFAULT NULL COMMENT "别名" AFTER title;
-ALTER TABLE `ot_com`.`ot_product_category` CHANGE `name` `name` VARCHAR(512) CHARSET utf8 COLLATE utf8_general_ci NOT NULL  COMMENT '分类名称';
--- Data/ot20170413.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE `ot_com`.`ot_operate_log` CHANGE `remark` `remark` VARCHAR(1024) CHARSET utf8 COLLATE utf8_general_ci DEFAULT ''  NOT NULL  COMMENT '日志备注';
